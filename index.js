ec3c685860a32456391e12f17dcb1058f6150108
NoahSolis <NSolis1993@gmail.com>
2022-03-27 21:52:16 -0500

    commit tree f3d512f65643c08330c5d7371dfc1ec1ed095930

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(functionsAreFun) {
+    functionsAreFun();
+  }
+
+function returnsANamedFunction() {
+    return function randomName() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        
+    }
+}
